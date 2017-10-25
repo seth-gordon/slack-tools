18a242fc0bea179645b9d2f187376954858e53ed
Tyler Kaminsky <tkaminsky@goscoutgo.com>
2017-10-25 09:57:52 -0400

    Slack Slash Routes (#3)
    Adding the base routes for the slack commands and the deploy-info command works.

--- slack-hooks.py
+++ slack-hooks.py
@@ -2,11 +2,22 @@
 # coding:utf-8
 # Copyright 2017 Scout Exchange, LLC. All Rights Reserved.
 from flask import Flask, jsonify, request
+import json
+import requests
 
 
 app = Flask(__name__)
 # we should test request tokens to make sure they came from slack.
 app.config['token'] = '3TXXJyr3EHFhDDMTqDT2TlPl'
+
+ENVIRONMENT_URLS = {
+    'blackops': ['https://blackops-connect.goscoutgo.com/deployment_info.json',
+                 'https://blackops.talentpx.com/deployment_info.json'],
+    'staging': ['https://staging-connect.goscoutgo.com/deployment_info.json',
+                'https://staging.talentpx.com/deployment_info.json'],
+    'demob': ['https://demob-connect.goscoutgo.com/deployment_info.json',
+              'https://demob.talentpx.com/deployment_info.json']
+}
 
 
 @app.route('/test-hook', methods=['POST'])
@@ -15,6 +26,64 @@ def test_hook():
         'text': 'test-hook response; *ngrok worked*!'
     }
     return jsonify(response)
+
+
+@app.route('/deploy', methods=['POST'])
+def slack_deploy():
+    response = {
+        'text': 'Deploying'
+    }
+    return jsonify(response)
+
+
+@app.route('/reload', methods=['POST'])
+def slack_reload():
+    response = {
+        'text': 'Reloading'
+    }
+    return jsonify(response)
+
+
+@app.route('/rollforward', methods=['POST'])
+def slack_rollforward():
+    response = {
+        'text': 'Rolling Forward'
+    }
+    return jsonify(response)
+
+
+@app.route('/scheduler', methods=['POST'])
+def slack_scheduler():
+    response = {
+        'text': 'Scheduler'
+    }
+    return jsonify(response)
+
+
+@app.route('/worker', methods=['POST'])
+def slack_worker():
+    response = {
+        'text': 'Worker'
+    }
+    return jsonify(response)
+
+
+@app.route('/deployment-info', methods=['POST'])
+def slack_deployment_info():
+    """
+    Fetches connect and tpx deployment info for a given environment.
+    """
+    # TODO: Need to add error handling in case the environment given doesn't
+    # exist in our dict.
+    text = request.form['text'].trim()
+    environment = text.split(' ')[0].lower()
+
+    connect = requests.get(ENVIRONMENT_URLS[environment][0])
+    tpx = requests.get(ENVIRONMENT_URLS[environment][1])
+    info = {'connect': json.loads(connect.text),
+            'tpx': json.loads(tpx.text)
+            }
+    return jsonify(info)
 
 
 if __name__ == '__main__':
